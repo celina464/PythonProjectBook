5a7fedc2adda2b24b2e4a0e7780f1d057a178cc5
Christian! <67934837+EverythingWillBeCode@users.noreply.github.com>
2023-05-04 09:15:38 -0300

    update

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -16,7 +16,7 @@ player_score = 0
 computer_score = 0
 
 def hangedman(hangman):
-    log.debug("hangman is %s", hangman)
+    #log.debug("hangman is %s", hangman)
     graphic = [
         """
             +-------+
@@ -25,111 +25,66 @@ def hangedman(hangman):
             |
             |
             |
-            =========
-        """,
-        """    
-            +-------+
-            |       |
-            |       0
-            |
-            |
-            =============
+            ==============
         """,
         """
             +-------+
             |       |
-            |       0
-            |       |
+            |       O
             |
-            =============
+            |
+            |
+            ==============
         """,
         """
             +-------+
             |       |
-            |       0
+            |       O
+            |       |
+            |
+            |
+            ==============
+        """,
+        """
+            +-------+
+            |       |
+            |       O
             |      -|
             |
-            =============
+            |
+            ==============
         """,
         """
             +-------+
             |       |
-            |       0
+            |       O
             |      -|-
             |
-            =============
+            |
+            ==============
         """,
         """
             +-------+
             |       |
-            |       0
+            |       O
             |      -|-
             |      /
-            =============
+            |
+            ==============
         """,
         """
             +-------+
             |       |
-            |       0
+            |       O
             |      -|-
-            |      / \
-            =============
-        """,]
+            |      / \\
+            |
+            ==============
+        """]
     print(graphic[hangman])
     return
 
-def start():
-    print ("Let's play a game of Linux Hangman.")
-    while game():
-        pass
-    scores()
 
-def game():  # sourcery skip: low-code-quality
-    dictionary = ["gnu", "kernel", "linux", "mageia", "penguin", "ubuntu"]
-    word = choice(dictionary)
-    word_length = len(word)
-    clue = word_length * ["_"]
-    tries = 6
-    letters_tried = ""
-    guesses = 0
-    letters_right = 0
-    letters_wrong = 0
-    global computer_score, player_score
-    
-    while (letters_wrong != tries) and ("".join(clue) != word):
-        letter=guess_letter()
-        if len(letter)==1 and letter.isalpha():
-            if letters_tried.find(letter) != -1:
-                print ("You've already picked"), letter
-            else:
-                letters_tried = letters_tried + letter
-                first_index = word.find(letter)
-                if first_index == -1:
-                    letters_wrong += 1
-                    print ("Sorry,", letter, "isn't what we're looking for.")
-                else:
-                    print ( "Congratulations,", letter, "is correct.")
-                    for i in range(word_length):
-                        if letter == word[i]:
-                            clue[i] = letter
-        else:
-            print ("Choose another.")
-        
-        hangedman(letters_wrong)
-        print (" ").join(clue)
-        print ("Guesses: "), letters_tried
-        
-        if letters_wrong == tries:
-            print ("Game Over.")
-            print ("The word was"), word
-            computer_score += 1
-            break
-        if "".join(clue) == word:
-            print ("You Win!")
-            print ("The word was"), word
-            player_score += 1
-            break
-        return play_again()
     
 def guess_letter():
     print
@@ -149,8 +104,63 @@ def play_again():
 def scores():
     global player_score, computer_score
     print ("HIGH SCORES")
-    print ("Player: "), player_score
-    print ("Computer: "), computer_score
+    print ("Player: ",player_score)
+    print ("Computer: ",computer_score)
+
+
+def start():
+    print ("Let's play a game of Linux Hangman.")
+    while game():
+        pass
+    scores()
+
+def game():  # sourcery skip: low-code-quality
+    dictionary = ["gnu"]
+    word = choice(dictionary)
+    word_length = len(word)
+    clue = word_length * [""]
+    tries = 6
+    letters_tried = ""
+    guesses = 0
+    letters_right = 0
+    letters_wrong = 0
+    global computer_score, player_score
+    
+    while (letters_wrong != tries) and ("".join(clue) != word):
+        letter=guess_letter()
+        if len(letter)==1 and letter.isalpha():
+            if letters_tried.find(letter) != -1:
+                print("You've already picked", letter)
+            else:
+                letters_tried = letters_tried + letter
+                first_index = word.find(letter)
+                if first_index == -1:
+                    letters_wrong += 1
+                    print ("Sorry,", letter, "isn't what we're looking for.")
+                else:
+                    print("Congratulations,", letter, "is correct.")
+                    for i in range(word_length):
+                        if letter == word[i]:
+                            clue[i] == letter
+        else:
+            print("Choose another.")
+        hangedman(letters_wrong)
+        print("".join(clue))
+        print("Guesses: ", letters_tried)
         
-    if __name__ == '__main__':
-        start()
+        if letters_wrong == tries:
+            print("Game Over.")
+            print("The word was", word)
+            computer_score += 1
+            break
+        if "".join(clue) == word:
+            print ("You Win!")
+            print ("The word was", word)
+            player_score += 1
+            break
+        
+    return play_again()
+
+
+if __name__ == '__main__':
+    start()
